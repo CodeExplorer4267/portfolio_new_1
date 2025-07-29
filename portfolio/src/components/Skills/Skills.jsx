497667ba06dee54de397a1ea6047b6876d7680d4
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-07-29 21:07:57 +0530

    commit14

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -22,18 +22,80 @@ const Skills = () => {
           logic and APIs, I leverage modern tools and frameworks to deliver
           efficient, scalable, and high-performance web applications.
         </p>
-        <div className="w-1/2 flex flex-row justify-center items-center flex-wrap gap-15 mt-[15px] text-white">
-          <div className="w-[250px] h-[250px] bg-[#050414] border-3 border-amber-300 rounded-[20px] p-5">
-            <h2 className="text-center text-2xl">{SkillsInfo[0].title}</h2>
+        <div className="w-[60%] flex flex-row justify-center items-center flex-wrap gap-15 mt-[15px] text-white">
+          <div className="w-[350px] h-[300px] bg-[#050414] border-3 border-amber-300 rounded-[40px] p-5">
+            <h2 className="text-center text-2xl mb-[20px] font-semibold">{SkillsInfo[0].title}</h2>
+            <div className="flex flex-row flex-wrap gap-[10px]">
+              {SkillsInfo[0].skills.map((skill, index) => {
+                return (
+                  <div key={index} className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-amber-300 rounded-[20px]">
+                    <img
+                      src={skill.logo}
+                      alt={skill.name}
+                      className="h-5 w-5"
+                    />
+                    <span className="text-white text-[13px]">{skill.name}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="w-[250px] h-[250px] bg-[#050414] border-3 border-amber-300">
-            Hello
+          <div className="w-[350px] h-[300px] bg-[#050414] border-3 border-amber-300 rounded-[40px] p-5">
+            <h2 className="text-center text-2xl mb-[20px] font-semibold">{SkillsInfo[1].title}</h2>
+            <div className="flex flex-row flex-wrap gap-[10px]">
+                {
+                  SkillsInfo[1].skills.map((skill,index)=>{
+                    return (
+                      <div key={index} className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-amber-300 rounded-[20px]">
+                        <img
+                          src={skill.logo}
+                          alt={skill.name}
+                          className="h-5 w-5"
+                        />
+                        <span className="text-white text-[13px]">{skill.name}</span>
+                      </div>
+                    )
+                  })
+                }
+            </div>
           </div>
-          <div className="w-[250px] h-[250px] bg-[#050414] border-3 border-amber-300">
-            Hello
+          <div className="w-[350px] h-[300px] bg-[#050414] border-3 border-amber-300 rounded-[40px] p-5">
+            <h2 className="text-center text-2xl mb-[20px] font-semibold">{SkillsInfo[2].title}</h2>
+            <div className="flex flex-row flex-wrap gap-[10px]">
+              {
+                SkillsInfo[2].skills.map((skill,index)=>{
+                  return (
+                    <div key={index} className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-amber-300 rounded-[20px]">
+                      <img
+                        src={skill.logo}
+                        alt={skill.name}
+                        className="h-5 w-5"
+                      />
+                      <span className="text-white text-[13px]">{skill.name}</span>
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
-          <div className="w-[250px] h-[250px] bg-[#050414] border-3 border-amber-300">
-            Hello
+          <div className="w-[350px] h-[300px] bg-[#050414] border-3 border-amber-300 rounded-[40px] p-5">
+            <h2 className="text-center text-2xl mb-[20px] font-semibold">{SkillsInfo[3].title}</h2>
+            <div className="flex flex-row flex-wrap gap-[10px]">
+              {
+                SkillsInfo[3].skills.map((skill,index)=>{
+                  return (
+                    <div key={index} className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-amber-300 rounded-[20px]">
+                      <img
+                        src={skill.logo}
+                        alt={skill.name}
+                        className="h-5 w-5"
+                      />
+                      <span className="text-white text-[13px]">{skill.name}</span>
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
       </motion.div>
