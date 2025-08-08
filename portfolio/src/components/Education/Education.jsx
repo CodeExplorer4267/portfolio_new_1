8f1386825b2cf07018032e28ee1719dc29cf8c8e
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-08 21:20:50 +0530

    commit29

--- portfolio/src/components/Education/Education.jsx
+++ portfolio/src/components/Education/Education.jsx
@@ -3,6 +3,7 @@ import "./Education.css";
 import GradientText from "../Animations/Gradient-Text";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { SkillsInfo } from "../../constants";
 const Education = () => {
   return (
     <div className="flex flex-col justify-center items-center pt-[20px] pb-[30px] w-[100%]">
@@ -14,7 +15,7 @@ const Education = () => {
       >
         <h1 className="text-4xl font-bold mb-[30px]">Education</h1>
       </GradientText>
-      <div className="flex flex-row justify-center items-center w-[90%] gap-[30px] h-[400px]">
+      <div className="flex flex-row justify-center items-center w-[90%] gap-[30px] h-[500px]">
         <div className="flex flex-row justify-center items-center h-[100%] w-[40%] p-[10px]">
           <img
             src="./school_logo.png"
@@ -66,7 +67,7 @@ const Education = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-center items-center w-[90%] gap-[30px] h-[400px]">
+      <div className="flex flex-row justify-center items-center w-[90%] gap-[30px] h-[600px]">
         <div className="flex flex-col justify-center items-center gap-[30px] h-[100%] w-[40%] p-[15px]">
           <h2 className="text-3xl bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent font-bold text-center">
             Maulana Abul Kalam Azad University Of Technology
@@ -80,6 +81,84 @@ const Education = () => {
           <div className="flex flex-row justify-center gap-[35px] items-center w-[80%] text-white text-[16px] ">
             <p className="font-semibold">B.Tech. in Information Technology</p>
             <p className="font-semibold">CGPA - 9.04</p>
+          </div>
+          <div className="flex flex-row justify-center items-center gap-[10px] p-[5px] text-white text-1xl w-[100%] flex-wrap text-center">
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[0].logo} alt="" className="h-[40px] w-auto"/>
+                <p>HTML</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[1].logo} alt="" className="h-[40px] w-auto"/>
+                <p>CSS</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[3].logo} alt="" className="h-[40px] w-auto"/>
+                <p>Java Script</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[4].logo} alt="" className="h-[40px] w-auto"/>
+                <p>React</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[8].logo} alt="" className="h-[40px] w-auto"/>
+                <p>Tailwind CSS</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[2].logo} alt="" className="h-[40px] w-auto"/>
+                <p>SASS</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="./framer_motion_logo.png" alt="" className="h-[35px] w-auto"/>
+                <p>Framer Motion</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[9].logo} alt="" className="h-[40px] w-auto"/>
+                <p>GSAP</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[10].logo} alt="" className="h-[30px] w-auto" />
+                <p>Material UI</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[1].skills[1].logo} alt="" className="h-[30px] w-auto" />
+                <p>Node Js</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[1].skills[2].logo} alt="" className="h-[30px] w-auto" />
+                <p>Express Js</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[1].skills[4].logo} alt="" className="h-[30px] w-auto" />
+                <p>MongoDB</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[1].skills[3].logo} alt="" className="h-[30px] w-auto" />
+                <p>My SQL </p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="cloudinary_logo.png" alt="" className="h-[30px] w-auto" />
+                <p>Cloudinary</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src={SkillsInfo[0].skills[6].logo} alt="" className="h-[30px] w-auto" />
+                <p>Redux</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="socket_Io_logo.png" alt="" className="h-[30px] w-auto" />
+                <p>Socket IO</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="os_logo.webp" alt="" className="h-[30px] w-auto" />
+                <p>Operating System</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="network_logo.jpg" alt="" className="h-[30px] w-auto" />
+                <p>Networks</p>
+             </div>
+             <div className="flex flex-col justify-center items-center gap-[5px]">
+                <img src="database_logo.avif" alt="" className="h-[30px] w-auto" />
+                <p>Database Management</p>
+             </div>
           </div>
         </div>
         <FaArrowAltCircleLeft className="text-purple-700 text-[50px] absolute right-[48.5%] top-[520%] z-10" />
