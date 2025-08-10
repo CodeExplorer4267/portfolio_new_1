d10916b68e6b46cfbb9661fcd0ad654714dd0393
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-10 20:31:58 +0530

    commit36

--- portfolio/src/components/ContactMe/Contact.jsx
+++ portfolio/src/components/ContactMe/Contact.jsx
@@ -4,15 +4,8 @@ import GradientText from "../Animations/Gradient-Text";
 const Contact = () => {
   return (
     <div className="h-[500px] w-[100%] flex flex-col items-center pb-[20px] ">
-      <GradientText
-        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-        animationSpeed={3}
-        showBorder={false}
-        className="custom-class"
-      >
-        <h1 className="text-4xl font-bold mb-[40px]">Let's Talk</h1>
-      </GradientText>
-      <div className="flex flex-row justify-evenly items-center gap-[40px]">
+        <h1 className="text-4xl font-bold mb-[40px] bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">Let's Talk</h1>
+      <div className="flex flex-row justify-evenly items-center gap-[300px]">
         <div className="w-[60%]">
           <DotLottieReact
             src="./mainscene.json"
@@ -21,10 +14,14 @@ const Contact = () => {
             height={"400px"}
           />
         </div>
-        <div className=" flex flex-col gap-[20px]">
-          <h2 className="bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent font-bold">
+        <div className=" flex flex-col gap-[20px] justify-center items-center">
+          <h2 className="bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent font-bold text-2xl">
             Let's Connect
           </h2>
+          <input type="text" name="" id="" placeholder="Enter name" className="w-[350px] border border-purple-500 p-[15px] rounded-2xl"/>
+          <input type="text" name="" id="" placeholder="Enter email" className="w-[350px] border border-purple-500 p-[15px] rounded-2xl"/>
+          <textarea type="text" name="" id="" rows={4} placeholder="Enter Your Thoughts" className="w-[350px] border border-purple-500 p-[15px] rounded-2xl"/>
+          <button className="h-[40px] w-[120px] p-[5px] rounded-2xl bg-linear-to-bl from-violet-500 to-fuchsia-500 text-black font-bold">Submit</button>
         </div>
       </div>
     </div>
