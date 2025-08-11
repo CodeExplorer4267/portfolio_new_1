48816131819b5644ca924b194305b2e9a65b6a5d
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-11 19:22:57 +0530

    commit40

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect,useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -12,8 +12,34 @@ import Achivement from "./components/Achivements/Achivement";
 import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/ContactMe/Contact";
 import Footer from "./components/Footer/Footer";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+  const lenis = useRef(null);
+
+  useEffect(() => {
+    // Initialize Lenis
+    lenis.current = new Lenis({
+      duration: 1.5, // Control the duration of the scroll
+      easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for smooth stop
+      smooth: true,
+      smoothTouch: true, // Enable smooth scrolling on touch devices
+    });
+
+    const animate = (time) => {
+      lenis.current.raf(time);
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+
+    // Cleanup on unmount
+    return () => {
+      lenis.current.destroy();
+    };
+  }, []);
+
+  
   return (
     <>
       <div className=" bg-[#050414] pt-[20px] min-h-screen w-full flex flex-col justify-center items-center gap-15">
--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -1,148 +1,146 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import ChromaGrid from "../Animations/Skills-grid";
 import { SkillsInfo } from "../../constants";
 import Tilt from "react-parallax-tilt";
 import GradientText from "../Animations/Gradient-Text";
 import "./Skills.css";
+
 const Skills = () => {
   return (
     <>
-    <section id="Skills">
-      <div
-        className="flex flex-col justify-center items-center pt-[30px] pb-[30px] gap-[30px] w-[100%]"
-      >
-        <GradientText
-          colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-          animationSpeed={3}
-          showBorder={false}
-          className="custom-class"
-        >
-          <h1 className="text-4xl font-bold">Skills</h1>
-        </GradientText>
-        <p className="text-1xl font-semibold text-white w-1/2">
-          As a passionate full stack developer, I have honed a diverse set of
-          skills across frontend and backend technologies. From crafting
-          responsive, user-friendly interfaces to building robust server-side
-          logic and APIs, I leverage modern tools and frameworks to deliver
-          efficient, scalable, and high-performance web applications.
-        </p>
-        <div className="w-[60%] flex flex-row justify-center items-center flex-wrap gap-15 mt-[15px] text-white">
-          <Tilt>
-            <motion.div 
-            className="card-animation ">
-              <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
-                <h2 className="text-center text-2xl mb-[20px] font-semibold">
-                  {SkillsInfo[0].title}
-                </h2>
-                <div className="flex flex-row flex-wrap gap-[10px]">
-                  {SkillsInfo[0].skills.map((skill, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
-                      >
-                        <img
-                          src={skill.logo}
-                          alt={skill.name}
-                          className="h-5 w-5"
-                        />
-                        <span className="text-white text-[13px]">
-                          {skill.name}
-                        </span>
-                      </div>
-                    );
-                  })}
+      <section id="Skills">
+        <div className="flex flex-col justify-center items-center pt-[30px] pb-[30px] gap-[30px] w-[100%]">
+          <GradientText
+            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+            animationSpeed={3}
+            showBorder={false}
+            className="custom-class"
+          >
+            <h1 className="text-4xl font-bold">Skills</h1>
+          </GradientText>
+            <p className="text-1xl font-semibold text-white w-1/2">
+            As a passionate full stack developer, I have honed a diverse set of
+            skills across frontend and backend technologies. From crafting
+            responsive, user-friendly interfaces to building robust server-side
+            logic and APIs, I leverage modern tools and frameworks to deliver
+            efficient, scalable, and high-performance web applications.
+          </p>
+          <div className="w-[60%] flex flex-row justify-center items-center flex-wrap gap-15 mt-[15px] text-white">
+            <Tilt>
+              <motion.div className="card-animation ">
+                <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
+                  <h2 className="text-center text-2xl mb-[20px] font-semibold">
+                    {SkillsInfo[0].title}
+                  </h2>
+                  <div className="flex flex-row flex-wrap gap-[10px]">
+                    {SkillsInfo[0].skills.map((skill, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
+                        >
+                          <img
+                            src={skill.logo}
+                            alt={skill.name}
+                            className="h-5 w-5"
+                          />
+                          <span className="text-white text-[13px]">
+                            {skill.name}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </motion.div>
+            </Tilt>
+            <Tilt>
+              <div className="card-animation">
+                <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
+                  <h2 className="text-center text-2xl mb-[20px] font-semibold">
+                    {SkillsInfo[1].title}
+                  </h2>
+                  <div className="flex flex-row flex-wrap gap-[10px]">
+                    {SkillsInfo[1].skills.map((skill, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
+                        >
+                          <img
+                            src={skill.logo}
+                            alt={skill.name}
+                            className="h-5 w-5"
+                          />
+                          <span className="text-white text-[13px]">
+                            {skill.name}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
-            </motion.div>
-          </Tilt>
-          <Tilt>
-            <div className="card-animation">
-              <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
-                <h2 className="text-center text-2xl mb-[20px] font-semibold">
-                  {SkillsInfo[1].title}
-                </h2>
-                <div className="flex flex-row flex-wrap gap-[10px]">
-                  {SkillsInfo[1].skills.map((skill, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
-                      >
-                        <img
-                          src={skill.logo}
-                          alt={skill.name}
-                          className="h-5 w-5"
-                        />
-                        <span className="text-white text-[13px]">
-                          {skill.name}
-                        </span>
-                      </div>
-                    );
-                  })}
+            </Tilt>
+            <Tilt>
+              <div className="card-animation">
+                <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
+                  <h2 className="text-center text-2xl mb-[20px] font-semibold">
+                    {SkillsInfo[2].title}
+                  </h2>
+                  <div className="flex flex-row flex-wrap gap-[10px]">
+                    {SkillsInfo[2].skills.map((skill, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
+                        >
+                          <img
+                            src={skill.logo}
+                            alt={skill.name}
+                            className="h-5 w-5"
+                          />
+                          <span className="text-white text-[13px]">
+                            {skill.name}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
-            </div>
-          </Tilt>
-          <Tilt>
-            <div className="card-animation">
-            <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
-              <h2 className="text-center text-2xl mb-[20px] font-semibold">
-                {SkillsInfo[2].title}
-              </h2>
-              <div className="flex flex-row flex-wrap gap-[10px]">
-                {SkillsInfo[2].skills.map((skill, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
-                    >
-                      <img
-                        src={skill.logo}
-                        alt={skill.name}
-                        className="h-5 w-5"
-                      />
-                      <span className="text-white text-[13px]">
-                        {skill.name}
-                      </span>
-                    </div>
-                  );
-                })}
+            </Tilt>
+            <Tilt>
+              <div className="card-animation">
+                <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
+                  <h2 className="text-center text-2xl mb-[20px] font-semibold">
+                    {SkillsInfo[3].title}
+                  </h2>
+                  <div className="flex flex-row flex-wrap gap-[10px]">
+                    {SkillsInfo[3].skills.map((skill, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
+                        >
+                          <img
+                            src={skill.logo}
+                            alt={skill.name}
+                            className="h-5 w-5"
+                          />
+                          <span className="text-white text-[13px]">
+                            {skill.name}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
-            </div>
-            </div>
-          </Tilt>
-          <Tilt>
-            <div className="card-animation"> 
-            <div className="w-[100%] h-[100%] bg-[#232129] rounded-[40px] p-5 z-99 relative">
-              <h2 className="text-center text-2xl mb-[20px] font-semibold">
-                {SkillsInfo[3].title}
-              </h2>
-              <div className="flex flex-row flex-wrap gap-[10px]">
-                {SkillsInfo[3].skills.map((skill, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="flex flex-row justify-start items-center p-[8px] gap-[5px] border-2 border-purple-600 rounded-[20px]"
-                    >
-                      <img
-                        src={skill.logo}
-                        alt={skill.name}
-                        className="h-5 w-5"
-                      />
-                      <span className="text-white text-[13px]">
-                        {skill.name}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            </div>
-          </Tilt>
+            </Tilt>
+          </div>
         </div>
-      </div>
       </section>
     </>
   );
